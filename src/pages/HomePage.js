e1557e01c4af6e80a11916b5cd58a21f7b185d2a
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-07-23 16:29:20 -0400

    Added and configured react routers

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>
+            This is a simple blog site that I built to learn React.
+        </p>
+        <p>
+            I hope you enjoy it!
+        </p>
+    </>
+);
+
+export default HomePage;
