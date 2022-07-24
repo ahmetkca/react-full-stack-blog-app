a781a1a2be7842a96e03fa33eb897891540a5f2b
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-07-24 00:37:44 -0400

    Added add comment and upvote functionality

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -6,14 +6,17 @@ const articlesInfo = {
     {
       name: "learn-react",
       upvotes: 0,
+      comments: [],
     },
     {
       name: 'learn-node',
       upvotes: 0,
+      comments: [],
     },
     {
       name: 'my-thoughts-on-resume',
       upvotes: 0,
+      comments: [],
     },
   ]
 }
@@ -22,7 +25,31 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/api/articles/:articleName/upvote', (req, res) => {
+  const articleName = req.params.articleName;
+  if (!articleName) return res.status(400).send("Please provide article name.");
 
+  const article = articlesInfo.articles.find(article => article.name === articleName);
+  if (!article) return res.status(404).send('Article is not found!');
+  console.log(`Article is found (${articleName})`);
+
+  article.upvotes++;
+  console.log(`The article named '${articleName}' now has ${article.upvotes} upvote.`);
+  res.status(200).send(article);
+});
+
+app.get('/api/articles', (_, res) => res.send(articlesInfo));
+
+
+app.post('/api/articles/:articleName/add-comment', (req, res) => {
+  const articleName = req.params.articleName;
+  if (!articleName) return res.status(400).send("Please provide article name.");
+  const article = articlesInfo.articles.find(article => article.name === articleName);
+  if (!article) return res.status(404).send('Article is not found!');
+  article.comments.push(req.body.comment);
+  console.log(`Adding comment to the article '${articleName}'.`);
+  res.status(200).send(article);
+})
 
 app.listen(8000, () => console.log('Listening on port 8000...'));
 
