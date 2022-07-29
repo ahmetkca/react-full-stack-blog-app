fbaa2d937e1de2cf9bc533a13c992a23ef9e4ec6
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-07-29 19:04:39 -0400

    Refactored MongoDb connection and created a context for it

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -11,6 +11,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const withDb = async (callback) => {
+  const client = new MongoClient(MONGO_CONNSTR, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db('my-blog');
+
+    await callback(db);
+  } catch (error) {
+    throw new Error(error);
+  } finally {
+    await client.close();
+  }
+}
+
 app.get('/hello/world', async (_, res) => {
   const client = new MongoClient(MONGO_CONNSTR, { useNewUrlParser: true });
   try {
@@ -25,97 +39,54 @@ app.get('/hello/world', async (_, res) => {
 })
 
 app.get('/api/articles', async (_, res) => {
-  const client = new MongoClient(MONGO_CONNSTR, { useNewUrlParser: true });
-  try {
-    await client.connect();
-    const db = client.db('my-blog');
+  withDb(async (db) => {
     const articles = await db.collection('articles').find({}).toArray();
     console.log(articles);
     res.status(200).json(articles);
-  } catch (error) {
-    res.status(500).json({ message: "Error: connection to mongodb.", error });
-  } finally {
-    await client.close();
-  }
-})
+  }).catch((error) => res.status(500).json({ message: "Error: during connection to mongodb.", error }));
+});
 
 app.get('/api/articles/:articleName', async (req, res) => {
-  const client = new MongoClient(MONGO_CONNSTR, { useNewUrlParser: true });
-  try {
-    const articleName = req.params.articleName;
-    await client.connect();
-    const db = client.db('my-blog');
-    const articleInfo = await db.collection('articles').findOne({ name: articleName });
-    if (!articleInfo) return res.status(404).json({ message: `The article with name ${articleName} not found.` });
-    res.status(200).json(articleInfo);
-  } catch (error) {
-    res.status(500).json({ message: "Error: connection to mongodb.", error });
-  } finally {
-    await client.close();
-  }
+  const articleName = req.params.articleName;
+  if (!articleName) return res.status(400).json({ message: "No article name provided. " });
+  withDb(async (db) => {
+    const filter = { name: articleName };
+    const article = await db.collection('articles').findOne(filter);
+    if (!article) return res.status(404).json({ message: `The article with name ${articleName} not found.` });
+    res.status(200).json(article);
+  }).catch((error) => res.status(500).json({ message: "Error: during connection to mongodb.", error }));
 })
 
 app.post("/api/articles/:articleName/upvote", async (req, res) => {
   const articleName = req.params.articleName;
   if (!articleName) return res.status(400).json({ message: "No article name provided. " });
 
-  const client = new MongoClient(MONGO_CONNSTR, { useNewUrlParser: true });
-  try {
-    await client.connect();
-    const db = client.db('my-blog');
-
+  withDb(async (db) => {
     const filter = { name: articleName };
-
-    const options = {};
-
-    const updateDoc = {
-      $inc: {
-        upvotes: 1,
-      }
-    };
-
-    const updateResult = await db.collection('articles').updateOne(filter, updateDoc, options);
-    console.log(
-      `${updateResult.matchedCount} document(s) matched the filter, updated ${updateResult.modifiedCount} document(s)`,
-    );
+    const updateDoc = { $inc: { upvotes: 1 } };
+    const updateResult = await db.collection('articles').updateOne(filter, updateDoc);
+    console.log(`${updateResult.matchedCount} document(s) matched the filter, updated ${updateResult.modifiedCount} document(s)`);
     if (updateResult.matchedCount === 0) return res.status(404).json({ message: `No article with name ${articleName} found` });
 
     const updatedArticle = await db.collection('articles').findOne({ name: articleName });
     res.status(200).json(updatedArticle);
-  } catch (error) {
-    res.status(500).json({ message: "Error: connection to mongodb.", error });
-  } finally {
-    await client.close();
-  }
+  }).catch((error) => res.status(500).json({ message: "Error: during connection to mongodb.", error }));
 })
 
 app.post('/api/articles/:articleName/add-comment', async (req, res) => {
   const articleName = req.params.articleName;
   if (!articleName) return res.status(400).json({ message: "No article name provided. " });
 
-  const client = new MongoClient(MONGO_CONNSTR, { useNewUrlParser: true });
-  try {
-    await client.connect();
-    const db = client.db('my-blog');
-
+  withDb(async (db) => {
     const filter = { name: articleName };
+    const updateDoc = { $push: { comments: req.body.comment } };
 
-    const updateDoc = {
-      $push: {
-        comments: req.body.comment,
-      },
-    };
-
-    const updateResult = await db.collection('articles').updateOne(filter, updateDoc, {});
+    const updateResult = await db.collection('articles').updateOne(filter, updateDoc);
     if (updateResult.matchedCount === 0) return res.status(404).json({ message: "No article with provided name found." });
 
     const updatedArticle = await db.collection('articles').findOne(filter);
     res.status(200).json(updatedArticle);
-  } catch (error) {
-    res.status(500).json({ message: "Error: cannot connect to mongodb.", error });
-  } finally {
-    await client.close();
-  }
+  }).catch((error) => res.status(500).json({ message: "Error: during connection to mongodb.", error }));
 })
 
 app.listen(8000, () => console.log('Listening on port 8000...'));
