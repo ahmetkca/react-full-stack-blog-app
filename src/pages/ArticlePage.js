aa723dc18223ac1ca6c8d14cda8cb86224f5e037
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-07-31 17:01:41 -0400

    Added component for upvoting and connected to backend

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import ArticlesList from '../components/ArticlesList.jsx';
 
-import NotFoundPage from './NotFoundPage.jsx';
+import { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
+import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
+
+import NotFoundPage from './NotFoundPage';
+
 
 import articlesContent from './articles-data.js';
 
 const ArticlePage = () => {
   const { articleName } = useParams();
-  const article = articlesContent.find(article => article.name === articleName);
 
-  if (!article) {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const blogApiUrl = `/api/articles/${articleName}`;
+    (async () => {
+      const result = await fetch(blogApiUrl,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      const body = await result.json();
+      setArticleInfo(body);
+    })()
+  }, [articleName]);
+
+  if (!articleInfo) {
     return <NotFoundPage />;
   }
+
   return (
     <div>
-      <h1>{article.name}</h1>
-      {article.content.map((paragraph, key) => (
-        <p key={key}>{paragraph}</p>
-      ))}
+      <h1>{articleInfo.name}</h1>
+      <UpvotesSection articleName={articleName} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
+      <CommentsList comments={articleInfo.comments} />
       <h3>Similar Articles</h3>
       <ArticlesList articles={articlesContent.filter(article => article.name !== articleName)} />
     </div>
